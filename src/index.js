1e4aba9140ad0fcb1cc419d3b2153301db68ff9b
justjum <jumasaurusrex@gmail.com>
2023-08-18 15:57:48 +1000

    Update of src and dist folders See previous commit message

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
-import "./style.css"
-import loadpage from "./page-load"
+import "./style.css";
+import loadpage from "./page-load";
 
 loadpage();
+
+class Task {
+    constructor(task, description, dueDate, priority, complete) {
+        this.task = task;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.complete = complete;
+    }
+        
+
+}
+
+
+// Array to store task objects
+const myTasks = [];
+console.log(myTasks);
+
+// Dummy tasks for testing
+myTasks.push(new Task("Eat", "Eat Lunch", "05/05/2023", "medium", false));
+myTasks.push(new Task("Run", "Get those legs working", "25/05/23", "high", false));
+console.log(myTasks[1].task);
+console.log(myTasks[0].priority);
+
+localStorage.setItem("My Tasks", myTasks);
+
+let addTask = () => {
+    
+}
+
--- src/page-load.js
+++ src/page-load.js
@@ -1,3 +1,34 @@
 export default function loadpage() {
-    console.log("hooked up");
+     buildLayout();  
+     buildSidebar(); 
+     buildContainer();
+}
+
+const buildLayout = () => {
+    const content = document.getElementById("content");
+    const sidebar = document.createElement("div");
+    const container = document.createElement("div");
+
+    sidebar.setAttribute("class", "sidebar");
+    sidebar.setAttribute("id", "sidebar");
+    container.setAttribute("class", "container");
+    container.setAttribute("id", "container");
+
+    content.appendChild(sidebar);
+    content.appendChild(container);
+}
+
+const buildSidebar = () => {
+
+}
+
+const buildContainer = () => {
+    const container = document.getElementById("container");
+    const taskForm = document.createElement('form');
+    taskForm.setAttribute("id", "task-form");
+    taskForm.setAttribute("name", "task-form")
+    taskForm.innerHTML = '<label>Task</label><input></input><label>Description</label><input></input><label>Due Date</label><input type="date"></input><label>Priority</label><select id="priority" name="priority"><option value="high">High</option><option value="medium">Medium</option><option value="low">Low</option></select><label>Complete?</label><select><option>Yes</option><option>No</option></select><button type="submit" for="task-form">Add Task</button>';
+
+    container.appendChild(taskForm);
+    
 }
